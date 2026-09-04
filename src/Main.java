776f5c05098932761d095159409ff9fe796b7b67
Jakob Kofoed Janot <jakob@janot.dk>
2026-09-04 13:56:01 +0200

    Tilføjet BMI beregner kode

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+void main() {
+    double bmi = calculateBmi(82.0, 1.79);
+    double iansBmi = calculateBmi(76, 1.90);
+
+    IO.println("Jakob, dit Body mass index er " + bmi + ". Og du er " + classifyBmi(bmi));
+    IO.println("Ian, dit Body mass index er " + iansBmi + ". Og du er " + classifyBmi(iansBmi));
+}
+
+double calculateBmi(double weight, double height) {
+    return weight / (height * height);
+}
+
+String classifyBmi(double bmi) {
+    if (isUnderWeight(bmi)) {
+        return "undervægtig";
+    } else if (isOverWeight(bmi)) {
+        return "overvægtig";
+    } else {
+        return "normalvægtig";
+    }
+}
+
+boolean isOverWeight(double bmi) {
+    return bmi >= 25;
+}
+
+boolean isUnderWeight(double bmi) {
+    return bmi <= 18.5;
+}
