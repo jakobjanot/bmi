dd5283225faf9438a6d10d8f14de637b6a7f00ea
Jakob Kofoed Janot <jakob@janot.dk>
2026-09-04 14:20:57 +0200

    Omnavngav du er

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ void main() {
     double bmi = calculateBmi(82.0, 1.79);
     double iansBmi = calculateBmi(76, 1.90);
 
-    IO.println("Jakob, dit BMI er " + bmi + ". Og du er " + classifyBmi(bmi));
-    IO.println("Ian, dit BMI er " + iansBmi + ". Og du er " + classifyBmi(iansBmi));
+    IO.println("Jakob, dit BMI er " + bmi + ". Du er " + classifyBmi(bmi));
+    IO.println("Ian, dit BMI er " + iansBmi + ". Du er " + classifyBmi(iansBmi));
 }
 
 double calculateBmi(double weight, double height) {
